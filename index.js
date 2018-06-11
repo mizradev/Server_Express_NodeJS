ac3abf48c95ba632749684a4c7d47492a90052cd
yosoymizrra <jorgemizrain@gmail.com>
2018-06-10 18:33:37 -0600

    Se enseña como se utilizan las RUTAS en express

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const express = require('express')
 // se realiza la instancia de Express
 const app = express()
 
+// Rutas
 app.get('/',(req,res)=>{
     res.end('HOla MUndo con Express')
 })
+
+app.get('/login',(req,res)=>{
+    res.end('Login');
+})
+
+app.get('*',(req,res)=>{
+    res.end('Esta recurso no existe');
+});
 
 app.listen(3000,()=>{console.log('Run Server on http://localhost:3000')})
