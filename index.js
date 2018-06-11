206c067054ea8cf4f21d2af1af52dafa99c9a4bb
yosoymizrra <jorgemizrain@gmail.com>
2018-06-10 19:19:48 -0600

    Los middleware en expres, el uso de morgan para ver informacion de los req en http de los proyectos en nodejs

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // importacion de express 
 const express = require('express')
+const morgan = require('morgan')
 // se realiza la instancia de Express
 const app = express()
+/*
+     uso de morgan -> que sirve para mostrar los log de req del http
+     parametros: dev, short,tiny,combined*, etc
+    url del modilo:https://www.npmjs.com/package/morgan
+*/
+app.use(morgan('dev'))
+
+//Middleware -> funciones que se activan con las rutas
+app.use((req,res,next)=>{
+    console.log('request url: '+req.url);
+    next();
+});
 
 // Rutas
 app.get('/',(req,res)=>{
@@ -15,5 +28,7 @@ app.get('/login',(req,res)=>{
 app.get('*',(req,res)=>{
     res.end('Esta recurso no existe');
 });
+
+
 
 app.listen(3000,()=>{console.log('Run Server on http://localhost:3000')})
